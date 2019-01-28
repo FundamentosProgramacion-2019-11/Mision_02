c48257a3e9aae4db61e9014c9c9e367c1ac0a37f
JuanCarlos-A01376511 <42353013+JuanCarlos-A01376511@users.noreply.github.com>
2019-01-28 12:42:53 -0600

    Update velocidad.py

--- velocidad.py
+++ velocidad.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Juan Carlos Flores García, A01376511
+# Descripcion: Programa que calcula la distancia y el tiempo que recorrió un auto.
 
 # Escribe tu programa después de esta línea.
+v = int(input("Teclea la velocidad del auto en k/m, v: "))
+
+distancia1 = v*6
+
+distancia2 = v*3.5
+
+tiempo1 = 485/v
+
+print("Velocidad del auto en k/m: %.1f" % v)
+print("Distancia recorrida en 6 horas: %.1f" % distancia1)
+print("Distancia recorrida en 3.5 horas: %.1f" % distancia2)
+print("Tiempo para recorrer 485km: %.1f" % tiempo1)
