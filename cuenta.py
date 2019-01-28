c0ab3750bc7442306c348935310abd3ec77185c3
kathdez7 <46934923+kathdez7@users.noreply.github.com>
2019-01-28 14:23:16 -0600

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,19 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Katia Hernández Barrera
+# Descripcion: Programa nqu calcula el total de la cuenta tomando en cuenta IVA y propina
 
 # Escribe tu programa después de esta línea.
+Sub = int (input("inserte el costo de la comida"))
+
+print ("costo de su comida", Sub)
+
+P = (Sub * 0.13)
+
+print ("Propina", P)
+
+I = (Sub * 0.16)
+
+print ("IVA", I)
+
+Total = Sub + P + I
+
+print ("Total a pagar", Total)
