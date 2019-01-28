c6c1f98f33822123f4e55264ae249d29bd9b7431
kathdez7 <46934923+kathdez7@users.noreply.github.com>
2019-01-27 21:17:10 -0600

    Update misDatos.py

--- misDatos.py
+++ misDatos.py
@@ -2,3 +2,8 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+print ("Nombre:","Katia Hernández Barrera")
+print ("Matrícula", "A01375906")
+print ("Carrera","LAD")
+print ("Escuela de procedencia", "PrepaTec", "Programa bicultural")
+print ("Descripción", "Me encanta la música, canto desde chica y toco el piano, amo bailar tap, dibujar y hacer animaciones")
