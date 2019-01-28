e5c89124e09c725ba673a10fb69d4a57bab26901
kathdez7 <46934923+kathdez7@users.noreply.github.com>
2019-01-28 14:24:57 -0600

    Update clase.py

--- clase.py
+++ clase.py
@@ -1,4 +1,19 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Katia Hernández Barrera
+# Descripcion: Programa quew calcula porcentaje de hobres y mujeres así como total de alumnos inscritos
 
 # Escribe tu programa después de esta línea.
+M = int (input("escriba el número de mujeres"))
+
+H = int (input("escriba el número de hombres"))
+
+Total = M+H
+
+print ("Total de alumnos", Total)
+
+PorcentajeM = (M*100)/Total
+
+print ("Mujeres inscritas", PorcentajeM)
+
+PorcentajeH = (H*100)/Total
+
+print ("Hombres inscritos", PorcentajeH)
