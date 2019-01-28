9e0bcd0bd745d9456a2157fed1fc31b9054ab643
JuanCarlos-A01376511 <42353013+JuanCarlos-A01376511@users.noreply.github.com>
2019-01-28 12:58:50 -0600

    Update clase.py

--- clase.py
+++ clase.py
@@ -1,4 +1,18 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Juan Carlos Flores García, A01376511
+# Descripcion: Programa que calcula el total de alumnos inscritos y el porcentaje de hombres y mujeres.
 
 # Escribe tu programa después de esta línea.
+mujeresNumero = float(input("Teclea el número de mujeres incritas: "))
+hombresNumero = float(input("Teclea el número de hombres inscritos: "))
+
+
+total = mujeresNumero + hombresNumero
+mujeresPorcentaje = mujeresNumero/total*100
+hombresPorcentaje = hombresNumero/total*100
+
+
+print("Mujeres Inscritas: ", mujeresNumero)
+print("Hombres Inscritos: ", hombresNumero)
+print("Total de inscritos: %d", total)
+print("Porcentaje de Mujeres: %.1f %" % mujeresPorcentaje)
+print("Porcentaje de Hombres: %.1f %" % hombresPorcentaje)
