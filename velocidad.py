803339596409853e85c0f1ae9c28156b90ed2495
kathdez7 <46934923+kathdez7@users.noreply.github.com>
2019-01-28 14:22:01 -0600

    Update velocidad.py

--- velocidad.py
+++ velocidad.py
@@ -1,4 +1,21 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Katia Hernández Barrera
+# Descripcion: Programa que calcula la distancia a partir de la velocidad del viaje
 
 # Escribe tu programa después de esta línea.
+v = int (input("inserte la velocidad en km/h, solo números enteros"))
+T2 = 3.5
+T1 = 6
+D3 = 485
+
+D1= v*T1
+
+print ("Distancia recorrida en 6 hrs",D1)
+
+D2= v*T2
+
+print ("Distancia recorrida en 3.5 hrs",D2)
+
+T3= D3/v
+
+print ("Tiempo al recorrer 485 km",T3)
+
