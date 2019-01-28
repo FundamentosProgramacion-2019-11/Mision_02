5662c7ac6bed700bfe8fe3cfa475228ba7f24e6c
JuanCarlos-A01376511 <42353013+JuanCarlos-A01376511@users.noreply.github.com>
2019-01-28 12:48:08 -0600

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Juan Carlos Flores García, A01376511
+# Descripcion: Programa que calcula la propina, iva y total a pagar.
 
 # Escribe tu programa después de esta línea.
+totalComida = int(input("Introduzca el precio de su comida: "))
+
+propina = totalComida*.13
+
+iva = totalComida*.16
+
+totalAPagar = totalComida + propina + iva
+
+print("Costo de su comida: ", totalComida)
+print("Propina: $ %.2f" % propina)
+print("IVA: $ %.2f" % iva)
+print("Total a pagar: $ %.2f" % totalAPagar) 
