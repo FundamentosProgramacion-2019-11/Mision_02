69ce46d9d69bbf59d3aaf2721d79d6a3770004b4
JuanCarlos-A01376511 <42353013+JuanCarlos-A01376511@users.noreply.github.com>
2019-01-28 12:32:25 -0600

    Update misDatos.py

--- misDatos.py
+++ misDatos.py
@@ -1,4 +1,17 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Juan Carlos Flores García, A01376511
+# Descripcion: Imprimir mis datos personales como nombre completo, matrícula, carrera, escuela de procedencia y descripción.
 
 # Escribe tu programa después de esta línea.
+
+print ("""Nombre:
+       Juan Carlos Flores García""")
+print ("""Matrícula:
+       A01376511""")
+print ("""Carrera:
+        Ingeniería en Sistemas Digitales y Robótica""")
+print ("""Escuela de Procedencia:
+       Prepa Tec, Programa Multicultural""")
+print ("""Descripción:
+Mis gustos son dibujar, jugar videojuegos y ver películas. Mis habilidades son hablar inglés bastante bien, tener conocimientos
+previos de robótica y mucha creatividad. Mis deportes favoritos son natación y Taekwondo. Mi libro favorito es el Retrato de Dorian Grey y
+he viajado a Texas en Estados Unidos.""")
