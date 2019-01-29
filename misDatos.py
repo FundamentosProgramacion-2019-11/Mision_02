362e97a25b91cb620b677ebe644a13f3a8402968
A01748311 <46821591+A01748311@users.noreply.github.com>
2019-01-29 17:27:20 -0600

    Santiago España Vázquez - MisDatos

--- misDatos.py
+++ misDatos.py
@@ -2,7 +2,7 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
-
+--------------------------------------------------------------------------------------
 # Autor: Santiago España Vázquez, A01748311
 # Descripcion: Imprimir datos solicitados
 
