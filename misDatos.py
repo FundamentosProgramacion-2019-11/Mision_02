f800c6fb8f1feb5fa2a1231054a3dd0473111cde
A01748311 <46821591+A01748311@users.noreply.github.com>
2019-01-29 17:10:21 -0600

    Santiago España Vázquez - Datos

--- misDatos.py
+++ misDatos.py
@@ -2,3 +2,20 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+
+# Autor: Santiago España Vázquez, A01748311
+# Descripcion: Imprimir datos solicitados
+
+print("""
+Nombre: 
+Santiago España Vázquez
+Matricula:
+A01748311
+Carrera:
+ISC
+Escuela de procedecia:
+UVM Hispano
+Descripción:
+Me gustan los videojuegos, y estos me han dado el gusto de programar.
+Estoy aprendiendo a tocar piano, me gusta tocar guitarra.
+""")
