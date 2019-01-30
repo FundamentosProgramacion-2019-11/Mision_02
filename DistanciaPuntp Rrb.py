3c636b55de298579919a2c5245be6cb3ae70315a
RafaelRbt <46898196+RafaelRbt@users.noreply.github.com>
2019-01-29 23:09:06 -0600

    Add files via upload

---
+++ DistanciaPuntp Rrb.py
@@ -0,0 +1,9 @@
+#Rafael Romero Bello A01747730
+#Este programa saca raices de dos puntos
+x1=float(input("inserte x1:"))
+x2=float(input("inserte x2:"))
+y1=float(input("inserte y1:"))
+y2=float(input("inserte y2:"))
+d=((x2-x1)**2)+((y2-y1)**2)
+distancia=d**.5
+print("%.3f"%(distancia))
