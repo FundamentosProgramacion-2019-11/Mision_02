c05a5c176aacd8b3f15cc078ca7ec508b2a9df8b
RafaelRbt <46898196+RafaelRbt@users.noreply.github.com>
2019-01-29 18:12:22 -0600

    Update misDatos.py

--- misDatos.py
+++ misDatos.py
@@ -1,4 +1,12 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
-# Escribe tu programa después de esta línea.
+#Rafael Romero Bello A01747730
+#Este programa muestra los datos del
+N=(input("Nombre:"))
+M=(input("Martica"))
+C=(input("carrera"))
+E=(input("Escuela Anterior"))
+I=(input("Tus gustos, descripcion de ti,etc"))
+print("Nombre,:", N)
+print("Matricula:",M)
+print("carrera:",C)
+print("escuela anterior:",E)
+print("Descripcion breve:",I)
