ca1cc2a6bb84832a762f7ede8792a4973afe9ac4
RafaelRbt <46898196+RafaelRbt@users.noreply.github.com>
2019-01-29 20:26:41 -0600

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,8 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+#Rafael Romero Bello A01747730
+#Este programa te pone la cuenta total en un restaurante, incluyendo la propina y el iva.
+sub=float(input("inserte la cuenta inicial:"))
+iva=.16*sub
+prop=.13*sub
+total=(iva+prop)+sub
+print("el total es de: %.2f"%(total),"pesos")
 
-# Escribe tu programa después de esta línea.
