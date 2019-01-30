1910a15f31af562552b2f20c9066d3b9272e2a60
RafaelRbt <46898196+RafaelRbt@users.noreply.github.com>
2019-01-29 21:30:10 -0600

    Update clase.py

--- clase.py
+++ clase.py
@@ -1,4 +1,10 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
-# Escribe tu programa después de esta línea.
+#Rafael Romero Bello A01747730
+#Este programa saca el porcentaje de un grupo conformado por hombres y mujeres
+h=float(input("inserte el numero de hombres:"))
+m=float(input("inserte el numero de mujeres:"))
+t=h+m
+ph=(h/t)*100
+pm=(m/t)*100
+print("el total de personas es:",t)
+print("el porcentaje de hombres es de:%.2f"%(ph))
+print("el porcentaje de mujeres es de :%.2f"%(pm))
