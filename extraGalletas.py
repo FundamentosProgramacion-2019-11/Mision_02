36ab20e69140512789ad159fb919763e18c53d8a
ItzelCastro14 <46935783+ItzelCastro14@users.noreply.github.com>
2019-01-30 08:45:04 -0600

    Itzel Yanabany Castro Becerril, Grupo:03

---
+++ extraGalletas.py
@@ -0,0 +1,9 @@
+#Autor: Itzel Yanabany Castro Becerril
+#Calcular la cantidades necesarias de ingrdientes  para hacer galletas
+galletas=int(input("Cantidad de galletas para hacer: "))
+tazucar=(galletas*1.5)/48
+tmantequilla=(galletas*1)/48
+tharina=(galletas*2.75)/48
+print("Tazas de azucar: ",tazucar)
+print("Tazas de mantequilla: ",tmantequilla)
+print("Tazas de harina: ", tharina)
