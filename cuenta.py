f957a8524402751ce0b9eaf65a70b8a6b50f6e48
ItzelCastro14 <46935783+ItzelCastro14@users.noreply.github.com>
2019-01-29 18:07:54 -0600

    Itzel Yanabany Castro Becerril, Grupo; 03

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,10 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+#Autor:Itzel Yanabany Castro Becerril
+#Costo total de la cuenta
+cuenta=int(input("Costo de tu cuenta: "))
+propina=cuenta*0.13
+iva= cuenta*0.16
+total=cuenta+propina+iva
+print("Propina: %.2f"%(propina))
+print("IVA: %.2f" % (iva))
+print("Total a pagar: %.2f" % (total))
 
-# Escribe tu programa después de esta línea.
