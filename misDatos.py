29b584d74296ddbde639bd8e2efcebf1591c88ec
ItzelCastro14 <46935783+ItzelCastro14@users.noreply.github.com>
2019-01-29 18:08:51 -0600

    Itzel Yanabany Castro Becerril, Grupo: 03

--- misDatos.py
+++ misDatos.py
@@ -1,4 +1,13 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
-# Escribe tu programa después de esta línea.
+#Autor: Itzel Yanabany Castro Becerril
+# Mis datos Personales
+print(""" Nombre: 
+Itzel Yanabany Castro Becerril
+Matricula: 
+A01747242
+Carrera: 
+ISC
+Escuela de procedencia: 
+Preparatoria Oficial Numero 27
+Descripcion: 
+ Me gusta pasar tiempo con mis amigos, me gusta de todo timpo de musica excepto la banda, el rock y metal,
+ soy amante de los animales y me encantas las enchiladas.  """)
