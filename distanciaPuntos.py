5a038dd041552a2eb4e93f2a4abc8ee3fe13d88a
ItzelCastro14 <46935783+ItzelCastro14@users.noreply.github.com>
2019-01-29 18:06:12 -0600

    Itzel Yanabany Castro Becerril, Grupo: 03

---
+++ distanciaPuntos.py
@@ -0,0 +1,8 @@
+#Autor: Itzel Ynabany Castro Becerril
+#Calcular la distancia de dos puntos
+x1=int(input("x1: "))
+y1=int(input("y1: "))
+x2= int(input("x2: "))
+y2=int(input("y2: "))
+d=((x2-x1)**2 + (y2-y1)**2)**0.5
+print("Distancia: %.3f" %(d))
