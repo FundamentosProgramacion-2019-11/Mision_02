e10458ff50f8229bcc2cb748048e59b58747a64b
ItzelCastro14 <46935783+ItzelCastro14@users.noreply.github.com>
2019-01-29 18:10:45 -0600

    Itzel Yanabany Castro Becerril, Grupo: 03

--- clase.py
+++ clase.py
@@ -1,4 +1,11 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+#Autor: Itzel Yanabany Castro Becerril
+#Calcular el porcentaje de hombres y mujeres inscritos
+mujeres=int(input("Mujeres inscritas: "))
+hombre=int(input("Hombres inscritos: "))
+total= mujeres+hombre
+pmujeres=(mujeres*100) / total
+phombres=(hombre*100)/ total
+print("Total de inscritos: ", total)
+print("Porcentaje de mujeres: %.1f" %(pmujeres)) 
+print("Porcentaje de Hombres: %.1f" %(phombres))
 
-# Escribe tu programa después de esta línea.
