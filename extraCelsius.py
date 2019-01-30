1582b6dfec419293d335e17a901490ddf3ec2641
ItzelCastro14 <46935783+ItzelCastro14@users.noreply.github.com>
2019-01-30 08:46:04 -0600

    Itzel Yanabany Castro Becerril, Grupo:03

---
+++ extraCelsius.py
@@ -0,0 +1,6 @@
+
+#Autor: Itzel Yanabany Castro Becerril
+#cambiar las temperaturas Fahtrenheir a Celsius
+fahrenheit= int(input("Temperatura en Fahrenheir: "))
+celsius=(fahrenheit-32)*(5/2)
+print("La temperatura en Celsius es de : ",celsius)
