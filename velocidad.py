ccade618f3087bd285638e9536913235dd4e8e63
ItzelCastro14 <46935783+ItzelCastro14@users.noreply.github.com>
2019-01-29 18:09:59 -0600

    Itzel Yanabany Castro Becerril , Grupo: 03

--- velocidad.py
+++ velocidad.py
@@ -1,4 +1,12 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+#Autor: Itzel Yanabany Castro Becerril
+#Encontrar la velocidad
+v=int(input("Velocidad del auto en km/h : "))
+t1=6
+t2=3.5
+d1=v*t1
+d2=v*t2
+t=485/v
+print("Distancia recorrida en 6hrs: ", d1)
+print("Distancia recorrida en 3.5 hrs: ",d2)
+print("Tiempo para recorrer 485:  %.1f" %(t))
 
-# Escribe tu programa después de esta línea.
