2eda2c52c5596c1f955f648fadb130ff6af93eef
Iván <46821602+VictorIvanMr@users.noreply.github.com>
2019-01-30 21:26:13 -0600

    Víctor Iván Morales Ramos A01377601

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+#Víctor Iván Morales Ramos a01377601
+#analisis: Crear un algoritmo que calcule el precio total de una comida, recibiendo datos bases
 
-# Escribe tu programa después de esta línea.
+t = float(input("¿Cuál es el total de la comida en MXN?: "))
+
+p = t * 0.13
+iva = t * 0.16
+st = t
+tf = p + iva + st
+
+print("Subtotal: " , st)
+print("Propina: " , p)
+print("Importe al valor agregado (IVA): " , iva)
+print("Total: " , tf)
+
