33f5f8e453a38be847407d2d40cb90617b829318
LuisCarmonaV <46898166+LuisCarmonaV@users.noreply.github.com>
2019-01-30 23:53:36 -0600

    Luis Carmona Grpo2
    a01748395

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,10 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
+# Autor: Luis Adrian Carmona Villalobos, A01748395
+# Descripcion: Progama que calcula la cuenta
 # Escribe tu programa después de esta línea.
+Total = int(input("total de su comida"))
+propina= Total*.87
+IVA = Total*.84
+TotalReal = Total+propina+IVA
+print("Propina: ", propina)
+print("IVA: ", IVA)
+print("su Total fue: ", TotalReal)
