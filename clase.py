4f3c41361c93cb016251b094213249f110b5d830
LuisCarmonaV <46898166+LuisCarmonaV@users.noreply.github.com>
2019-01-30 23:59:59 -0600

    Luis grupo 2

--- clase.py
+++ clase.py
@@ -1,4 +1,11 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Luis Adrian A01748395
+# Descripcion: clase
 
 # Escribe tu programa después de esta línea.
+H =int(input("numero de mujeres:"))
+M = int(input("numero de homres: "))
+Total = M+H
+porM = (M*100)/H
+porH = (H*100)/M
+print("el % de hombres fue:", porH)
+print("el % de mujeres fue:", porM)
