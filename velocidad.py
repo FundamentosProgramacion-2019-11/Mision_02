ca4e83f4e13a27658731c7b3b1c1c1d06228dd83
LuisCarmonaV <46898166+LuisCarmonaV@users.noreply.github.com>
2019-01-30 23:46:00 -0600

    Luis Carmona Grupo2
    a01748395

--- velocidad.py
+++ velocidad.py
@@ -1,4 +1,8 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Luis Adrian Carmona Villalobos, A01748395
+# Descripcion: Progama para saber la velocidad de un auto la distancia recorrida en cierto tiempo y el tiempo para recorrer cierta distancia
 
 # Escribe tu programa después de esta línea.
+d= float(input("cual fue su distancia en km"))
+t=float(input("cual fue tiempo de recorrido en horas y minutos"))
+V = d/t
+print("Velocidad del auto en km/h: %.2f" %(V))
