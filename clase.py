13e5b0c4dce9cbc716e96a91952c9511ef3b88ee
Iván <46821602+VictorIvanMr@users.noreply.github.com>
2019-01-30 21:35:42 -0600

    Víctor Iván Morales Ramos A01377601

--- clase.py
+++ clase.py
@@ -1,4 +1,13 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+#Víctor Iván Morales Ramos a01377601
+#analisis: Crear un algoritmo que calcule la cantidad porcentual de hombres y mujeres en una clase.
 
-# Escribe tu programa después de esta línea.
+m = float(input("Cuantas mujeres hay en la clase: "))
+h = float(input("Cuantos hombres hay en la clase: "))
+
+t = m + h
+pm = (m / t) * 100
+ph = (h / t) * 100
+
+print("El total de alumnos es:" , t)
+print("El porcentaje de mujeres en el salon es: %" , pm )
+print("El porcentaje de hombres en el salon es: %" , ph)
