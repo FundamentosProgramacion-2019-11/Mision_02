1208699d600b9552997bae3b794b20151d17f7ae
Iván <46821602+VictorIvanMr@users.noreply.github.com>
2019-01-30 22:00:05 -0600

    Víctor Iván Morales Ramos A01377601

---
+++ ExtraTemperaturas.py
@@ -0,0 +1,8 @@
+#Víctor Iván Morales Ramos a01377601
+#analisis: Realizar conversion de F° a C°
+
+f = float(input("Dame tus grados en la escala Fahrenheit: "))
+
+c = (f-32) / 1.8
+
+print("Grados Celsius: %.2f" % (c))
