b9051b1edeee3162ec4cb5d1beb4377216516a3d
Sofía Daniela <46821612+SofiaDaniela@users.noreply.github.com>
2019-01-30 20:53:34 -0600

    Sofía Daniela Méndez Sandoval, A01242259, Grupo 03

--- misDatos.py
+++ misDatos.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Sofía Daniela Méndez Sandoval, A01242259
+# Descripcion: Mi información.
 
-# Escribe tu programa después de esta línea.
+#Mi información
+
+print("Nombre: "
+      "Sofía Daniela Méndez Sandoval ")
+print("Matrícula: "
+      "A01242259")
+print("Carrera: "
+      "LCMD")
+print("Escuela de Procedencia: "
+      "Secundaria y Preparatoria Tec de Monterrey, Programa Bicultural")
+print("Descripción:"
+      "Soy una norteña haciéndose pasar por chilanga. Nací en Ciudad Juárez y hace cuatro años y medio me mudé al Estado de México junto con toda mi familia. Me gusta mucho patinar en suelo y hacer actividades relacionadas con mi carrera.")
+
