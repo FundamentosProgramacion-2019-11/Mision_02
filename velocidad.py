a308f5da3228fc3bc1ba56475d273abdedca20ab
Sofía Daniela <46821612+SofiaDaniela@users.noreply.github.com>
2019-01-30 23:00:21 -0600

    Sofía Daniela Méndez Sandoval, A01242259, Grupo 03

--- velocidad.py
+++ velocidad.py
@@ -1,4 +1,17 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Sofía Daniela Méndez Sandoval, A01242259
+# Descripción: Calcula la velocidad
 
-# Escribe tu programa después de esta línea.
+velocidad = int(input("¿A cuántos km/h viaja el auto? (en números enteros): "))
+
+tiempo = 6
+tiempo2 = 3.5
+distancia = 485
+
+d = velocidad*tiempo
+d2 = velocidad*tiempo2
+t = distancia/velocidad
+
+print("Velocidad del auto: ")
+print("Distancia recorrida en 6 horas: ", "%.1f" % d, "km")
+print("Distancia recorrida en 3.5 hotas: ", "%.1f" % d2, "km")
+print("Tiempo para recorrer 485km: ", "%.1f" % t, "hrs")
