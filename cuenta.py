af930e99a7b08938f55359754f5d4cea0c58e5fc
JoseTec2000 <46898182+JoseTec2000@users.noreply.github.com>
2019-01-30 20:29:21 -0600

    José Isidro Sánchez Vázquez Grupo:02
    Hice los ejercicios extra

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,12 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: José Isidro Sánchez Vázquez, A01748144
+# Descripcion: Programa para calcular el costo total en un restaurante
 
 # Escribe tu programa después de esta línea.
+
+c=float(input("Costo de la comida: "))
+p1=c*.13
+p2=c*.16
+ct=p1+p2+c
+print("El valor de la propina es:%.2f"%(p1))
+print("El valor del IVA es:%.2f"%(p2))
+print("El valor total es:%.2f"%(ct),"pesos")
--- misDatos.py
+++ misDatos.py
@@ -1,4 +1,12 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor:José Isidro Sánchez Vázquez, A01748144
+# Descripcion: Datos personales
 
 # Escribe tu programa después de esta línea.
+
+print("Nombre: José Isidro Sánchez Vázquez")
+print("Matricula: A01748144")
+print("Carrera: ISDR")
+print("Escuela de procedencia: UVM Hispano")
+print("Gustos: \n Me gusta hacer deportes de contacto como lo es el box, karate, entre otros\n "
+      "pero tambien me guta la natacion y el soccer aparte de los deportes me gusta mucho los video juegos\n"
+      " y el anime tambien aprender mas de tecnologia ")
