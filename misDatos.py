133448ca9ec08cbe68a62dd8c77e16cf5a1091b7
Iván <46821602+VictorIvanMr@users.noreply.github.com>
2019-01-30 20:24:56 -0600

    Víctor Iván Morales Ramos A01377601

--- misDatos.py
+++ misDatos.py
@@ -1,4 +1,12 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+#Víctor Iván Morales Ramos A01377601
 
-# Escribe tu programa después de esta línea.
+#Analisis: Se presenta un algoritmo que muestra la información general del alumno#
+
+print("Nombre: Víctor Iván Morales Ramos")
+print("Matricula: A01377601 ")
+print("Carrera: Ingeniería en sistemas digitales y robótica")
+print("Escuela de procedencia: Prepa tec")
+print("Soy una persona muy curiosa, alegre y amigable. Me gustan mucho los videojuegos y"
+      " todo lo que implica, desde fabricarlos visualmente hasta jugarlos. en general "
+      "me considero alguien que nunca se conforma, que siempre quiere ir por algo mas "
+      "grande.")
