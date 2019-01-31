3421539ec9d43a49d6c3ba4e71d08c6cc1ddc18c
Sofía Daniela <46821612+SofiaDaniela@users.noreply.github.com>
2019-01-30 22:29:26 -0600

    Sofía Daniela Méndez Sandoval, A01242259, Grupo 03

---
+++ extraTemperaturas.py
@@ -0,0 +1,8 @@
+# Autor: Sofía Daniela Méndez Sandoval, A01242259
+# Descripción: Temperatura de °F a °C
+
+f = int(input("Ingresa temperatura en ºF: "))
+
+c = (f-32)*0.5555555556
+
+print(("La temperatura en ºC es: "), "%.1f" % c)
