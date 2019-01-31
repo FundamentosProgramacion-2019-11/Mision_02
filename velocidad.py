3d35dcf911abdc7a1815015dc93f995951ccb8e3
Iván <46821602+VictorIvanMr@users.noreply.github.com>
2019-01-30 20:48:05 -0600

    Víctor Iván Morales Ramos A01377601

--- velocidad.py
+++ velocidad.py
@@ -1,4 +1,17 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+#Víctor Iván Morales Ramos A01377601
 
-# Escribe tu programa después de esta línea.
+#Análisis: Se requiere un algoritmo que haga las operaciones necesarias para obtener los datos de desplazamiento de un auto recibiendo solo datos base.
+
+v = float(input("dame la velocidad a la que viaja el auto en km/h: "))
+
+t1 = 6
+t2 = 3.5
+d = 485
+
+dp1 = t1 * v
+dp2 = t2 * v
+tp1 = d / v
+
+print("La distancia recorrida en km que recorre el auto en 6 horas es: " , dp1)
+print("La distancia recorrida en km que recorre el auto en 3.5 horas es: " , dp2)
+print("El tiempo en horas y minutos que requiere para recorrer 485 km es: " , tp1)
